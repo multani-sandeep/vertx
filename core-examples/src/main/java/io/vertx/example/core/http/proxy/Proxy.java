64ca3a229a4563c373eb04952113fc544e68e89e
sandeepsingh <s_multani@yahoo.com>
2018-05-24 11:35:36 +0100

    Added ProxyChain

--- core-examples/src/main/java/io/vertx/example/core/http/proxy/Proxy.java
+++ core-examples/src/main/java/io/vertx/example/core/http/proxy/Proxy.java
@@ -11,34 +11,38 @@ import io.vertx.example.util.Runner;
  */
 public class Proxy extends AbstractVerticle {
 
-  // Convenience method so you can run it in your IDE
-  public static void main(String[] args) {
-    Runner.runExample(Proxy.class);
-  }
+	// Convenience method so you can run it in your IDE
+	public static void main(String[] args) {
+		System.out.println(">>>>" + args);
+		Runner.runExample(Proxy.class);
+	}
 
-  @Override
-  public void start() throws Exception {
-    HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-    vertx.createHttpServer().requestHandler(req -> {
-      System.out.println("Proxying request: " + req.uri());
-      HttpClientRequest c_req = client.request(req.method(), 8282, "localhost", req.uri(), c_res -> {
-        System.out.println("Proxying response: " + c_res.statusCode());
-        req.response().setChunked(true);
-        req.response().setStatusCode(c_res.statusCode());
-        req.response().headers().setAll(c_res.headers());
-        c_res.handler(data -> {
-          System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
-          req.response().write(data);
-        });
-        c_res.endHandler((v) -> req.response().end());
-      });
-      c_req.setChunked(true);
-      c_req.headers().setAll(req.headers());
-      req.handler(data -> {
-        System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
-        c_req.write(data);
-      });
-      req.endHandler((v) -> c_req.end());
-    }).listen(8080);
-  }
+	@Override
+	public void start() throws Exception {
+		
+		final int destport = 8282;
+		final int proxyport = 8080;
+		HttpClient client = vertx.createHttpClient(new HttpClientOptions());
+		vertx.createHttpServer().requestHandler(req -> {
+			System.out.println("Proxying request: " + req.uri());
+			HttpClientRequest c_req = client.request(req.method(), destport, "localhost", req.uri(), c_res -> {
+				System.out.println("Proxying response: " + c_res.statusCode());
+				req.response().setChunked(true);
+				req.response().setStatusCode(c_res.statusCode());
+				req.response().headers().setAll(c_res.headers());
+				c_res.handler(data -> {
+					System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
+					req.response().write(data);
+				});
+				c_res.endHandler((v) -> req.response().end());
+			});
+			c_req.setChunked(true);
+			c_req.headers().setAll(req.headers());
+			req.handler(data -> {
+				System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
+				c_req.write(data);
+			});
+			req.endHandler((v) -> c_req.end());
+		}).listen(proxyport);
+	}
 }
